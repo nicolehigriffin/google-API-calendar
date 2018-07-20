1d6c75653ead0523b2fd528c049bf52e187d5c2f
nicolehigriffin <35984384+nicolehigriffin@users.noreply.github.com>
2018-07-20 12:50:10 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -9,15 +9,16 @@ from oauth2client import file, client, tools
 import datetime
 
 # Setup the Calendar API
-SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+SCOPES = 'https://www.googleapis.com/auth/calendar'
 store = file.Storage('credentials.json')
 creds = store.get()
 if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES) #replcae client_secret.json with the downloaded json file
+    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
     creds = tools.run_flow(flow, store)
-service = build('calendar', 'v3', http=creds.authorize(Http()))
+service = discovery.build('calendar', 'v3', http=creds.authorize(Http()))
 
-# Call the Calendar API
+# Call the Calendar API -- first 10 events example
+'''
 now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
 print('Getting the upcoming 10 events')
 events_result = service.events().list(calendarId='primary', timeMin=now,
@@ -30,3 +31,20 @@ if not events:
 for event in events:
     start = event['start'].get('dateTime', event['start'].get('date'))
     print(start, event['summary'])
+'''
+
+timeZone = America/New_York
+EVENT = {
+    'summary':
+    'start':
+    'end':
+    'location':
+    'recurrence':
+    'description': 
+    'attendees': [
+        {'email':'#tutor_email'},
+        {'email':'#tutoree_email'},
+    ]
+
+#iframe code: <iframe src="https://calendar.google.com/calendar/embed?src=nyu.edu_vtl14t5liim30tkgricmkeonlc%40group.calendar.google.com&ctz=America%2FNew_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+#html code: <iframe src="https://calendar.google.com/calendar/b/1/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=nyu.edu_vtl14t5liim30tkgricmkeonlc%40group.calendar.google.com&amp;color=%23865A5A&amp;ctz=America%2FNew_York" style="border-width:0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
